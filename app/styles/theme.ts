c45eb17bf0819752d54ad7097417579da1c2a944
ljh0608 <m98m@catholic.ac.kr>
2023-12-11 13:26:02 +0900

    init: emotion, eslint, prettier 초기세팅

---
+++ app/styles/theme.ts
@@ -0,0 +1,11 @@
+import { Theme } from '@emotion/react';
+
+const theme: Theme = {
+  color: {
+    grey: '#9E9E9E',
+    darkGrey: '#757575',
+    lightGrey: '#BDBDBD',
+  },
+};
+
+export default theme;
