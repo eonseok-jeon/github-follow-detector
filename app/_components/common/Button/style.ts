57cbdf2fbc2776126a8c6bba56ec053e6844adc8
Jeon Eonseok <jseokeon@gmail.com>
2023-12-13 16:41:04 +0900

    style: 재사용 할 수 있는 Button component 생성
     #3

---
+++ app/_components/common/Button/style.ts
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+const CommonButton = styled.button`
+  display: inline-block;
+  padding: 0.5rem;
+  margin-bottom: 0.5rem;
+  border: 1px solid black;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.lightGrey};
+  transition: all 0.3s ease;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.grey};
+  }
+`;
+
+export { CommonButton };
