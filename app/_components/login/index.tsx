55397c43b9206ae2f298f6a76634763ae6dbb894
Jeon Eonseok <jseokeon@gmail.com>
2023-12-13 16:47:05 +0900

    style: UI 일부 구현
    1. form UI 구현
    1-1. input창 생성
    1-2. form sumbit button 생성
    2. 기존 버튼에 common Button component 적용
    
    #3

--- app/_components/login/index.tsx
+++ app/_components/login/index.tsx
@@ -1,27 +1,51 @@
+/** @jsxImportSource @emotion/react */
+
 'use client';
 
 import Container from '../common/Container';
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import Button from '../common/Button';
 
-const GitHubLink = styled.a`
-  padding: 0.5rem;
-  border: 1px solid black;
-  border-radius: 8px;
-  background-color: ${({ theme }) => theme.colors.lightGrey};
-  color: black;
-  transition: all 0.3s ease;
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
 
-  &:hover {
-    background-color: ${({ theme }) => theme.colors.grey};
+const FormInput = styled.input`
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid ${({ theme }) => theme.colors.grey};
+  border-radius: 5px;
+  font-size: 1.6rem;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.lightGrey};
   }
+`;
+
+const warningStyle = css`
+  font-size: 1.4rem;
+  margin-bottom: 1rem;
 `;
 
 const Login = () => {
   return (
     <Container>
-      <GitHubLink href="https://github.com/settings/tokens" target="_blank">
-        GitHub Token 만들러 가기
-      </GitHubLink>
+      <Button>
+        <a href="https://github.com/settings/tokens" target="_blank">
+          GitHub Token 만들러 가기
+        </a>
+      </Button>
+      <p css={warningStyle}>!토큰 발급시 권한 user(Update ALL user data)를 체크해주세요!</p>
+      <LoginForm>
+        <FormInput type="text" placeholder="GitHub Token을 입력해주세요" />
+        <Button type="submit">나의 맞팔 확인하기</Button>
+      </LoginForm>
     </Container>
   );
 };
