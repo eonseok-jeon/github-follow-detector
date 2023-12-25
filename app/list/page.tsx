e905c3232c9c7c554bc97479a394c8c5bda6c7a1
Jeon Eonseok <jseokeon@gmail.com>
2023-12-25 19:13:04 +0900

    style: follower list height 지정
    1. max-height 설정 후 overflow시 scroll 하도록 구현
    2. scroll css hide
    3. container name 변경
    
    #6

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -2,6 +2,7 @@
 
 'use client';
 
+import Link from 'next/link';
 import Image from 'next/image';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
@@ -9,12 +10,11 @@ import Button from '@/_components/common/Button';
 import Container from '@/_components/common/Container';
 import { CommonContainer } from '@/_components/common/Container/style';
 import { flexCenter, flexColumn, flexEnd, flexStart } from '@/_styles/reusableStyle';
-import Link from 'next/link';
 
 const ListPage = () => {
   return (
     <>
-      <ListContainer>
+      <MyInfoContainer>
         <Image css={userImage} src="/egg_princess.png" alt="user-profile-img" width={100} height={100} />
         <div css={userInfo}>
           <p>simeunseo</p>
@@ -24,7 +24,7 @@ const ListPage = () => {
             <span>팔로잉: 60</span>
           </div>
         </div>
-      </ListContainer>
+      </MyInfoContainer>
       <div css={followerButton}>
         <Button>맞팔 아닌 사람</Button>
         <Button>맞팔 확인하기</Button>
@@ -32,7 +32,7 @@ const ListPage = () => {
           <Link href="/">뒤로 가기</Link>
         </Button>
       </div>
-      <Container>
+      <ListContainer>
         <div css={selectRadio}>
           <InputRadio data-after="모두 선택" id="check-all" type="radio" defaultChecked name="select-check-option" />
           <InputRadio data-after="모두 해지" id="uncheck-all" type="radio" name="select-check-option" />
@@ -53,6 +53,27 @@ const ListPage = () => {
             </label>
           </FollowerItem>
           <FollowerItem>
+            <label css={followerInfo} htmlFor="follower2">
+              <Image css={userImage} src="/egg_princess.png" alt="user-profile-img" width={50} height={50} />
+              <span>easlaw80</span>
+              <InputCheckBox id="follower2" type="checkbox" />
+            </label>
+          </FollowerItem>
+          <FollowerItem>
+            <label css={followerInfo} htmlFor="follower2">
+              <Image css={userImage} src="/egg_princess.png" alt="user-profile-img" width={50} height={50} />
+              <span>easlaw80</span>
+              <InputCheckBox id="follower2" type="checkbox" />
+            </label>
+          </FollowerItem>
+          <FollowerItem>
+            <label css={followerInfo} htmlFor="follower2">
+              <Image css={userImage} src="/egg_princess.png" alt="user-profile-img" width={50} height={50} />
+              <span>easlaw80</span>
+              <InputCheckBox id="follower2" type="checkbox" />
+            </label>
+          </FollowerItem>
+          <FollowerItem>
             <label css={followerInfo} htmlFor="follower3">
               <Image css={userImage} src="/egg_princess.png" alt="user-profile-img" width={50} height={50} />
               <span>easlaw80</span>
@@ -60,7 +81,7 @@ const ListPage = () => {
             </label>
           </FollowerItem>
         </ul>
-      </Container>
+      </ListContainer>
     </>
   );
 };
@@ -122,10 +143,11 @@ const selectRadio = css`
   margin-bottom: 2rem;
 `;
 
-const ListContainer = styled(CommonContainer)`
+const MyInfoContainer = styled(CommonContainer)`
   ${flexStart}
 
   gap: 3rem;
+  margin-top: 3rem;
 `;
 
 const InputRadio = styled.input`
@@ -172,5 +194,17 @@ const InputCheckBox = styled.input`
     text-align: center;
     content: attr(data-after);
     font-size: 1.5rem;
+  }
+`;
+
+const ListContainer = styled(CommonContainer)`
+  max-height: calc(100vh - 17.6rem - 7.7rem - 3rem); // 전체 높이 - my-info - 3개 버튼 높이 - margin-bottom
+  overflow-y: auto;
+
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera*/
   }
 `;
